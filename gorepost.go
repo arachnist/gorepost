fefd916dcc8369bb314c437a0c106388c9db1254
Robert Gerus <arachnist@i.am-a.cat>
2015-10-15 21:04:24 +0200

    Yay, we can now read our own configuration file.

--- gorepost.go
+++ gorepost.go
@@ -1,13 +1,21 @@
-package gorepost
+package main
 
 import (
-    "log"
+    //"log"
+    "fmt"
     "os"
-    "time"
+    //"time"
 
     "github.com/arachnist/gorepost/config"
-    "github.com/sorcix/irc"
+    //"github.com/sorcix/irc"
 )
 
 func main() {
+    config, err := config.ReadConfig(os.Args[1])
+    if err != nil {
+        fmt.Println("Error reading configuration from", os.Args[1], "error:", err.Error())
+        os.Exit(1)
+    }
+
+    fmt.Println("My nickname:", config.Nick)
 }
