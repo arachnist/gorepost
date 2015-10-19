5e6591591f96639dd3cb3d2bcc6746ce1d396dff
Robert Gerus <arachnist@i.am-a.cat>
2015-10-19 10:37:08 +0200

    I think these things should work, but yeah… not yet tested.

--- gorepost.go
+++ gorepost.go
@@ -1,14 +1,89 @@
 package main
 
 import (
-    //"log"
+    "log"
     "fmt"
     "os"
-    //"time"
+    "time"
+    "math/rand"
 
     "github.com/arachnist/gorepost/config"
-    //"github.com/sorcix/irc"
+    "github.com/sorcix/irc"
 )
+
+type Connection struct {
+    Network     string
+    Input       chan irc.Message
+    Output      chan irc.Message
+    IRCConn     *irc.Conn
+    QuitSend    chan struct{}
+    QuitRecv    chan struct{}
+}
+
+func (c *Connection) Sender() {
+    for {
+        select {
+        case msg := <-c.Input:
+            c.IRCConn.Encode(&msg)
+        case <-c.QuitSend:
+            log.Println(c.Network, "closing Sender")
+            close(c.Input)
+            close(c.QuitSend)
+            return
+        }
+    }
+}
+
+func (c *Connection) Receiver() {
+    for {
+        msg, err := c.IRCConn.Decode()
+        if err != nil {
+            log.Println(c.Network, "error decoding message", err.Error())
+        }
+        select {
+        case c.Output <- *msg:
+        case <-c.QuitRecv:
+            log.Println(c.Network, "closing receiver")
+            close(c.Output)
+            close(c.QuitRecv)
+            return
+        }
+    }
+}
+
+func SetupConn(network string, config config.Config, connection *Connection) (error) {
+    rand.Seed(time.Now().UnixNano())
+    server := config.Servers[network][rand.Intn(len(config.Servers[network]))]
+
+    conn, err := irc.Dial(server)
+    if err != nil {
+        log.Println("Cannot connect to", network, "server:", server, "error:", err.Error())
+        return err
+    }
+    connection.IRCConn = conn
+
+    go connection.Sender()
+    go connection.Receiver()
+
+    // Initial commands sent to IRC server
+    connection.Input <- irc.Message{
+        Command:    "NICK",
+        Trailing:   config.Nick,
+    }
+    connection.Input <- irc.Message{
+        Command:    "USER",
+        Params:     []string{config.Nick, "3", "*"},
+        Trailing:   config.Nick,
+    }
+
+    return nil
+}
+
+func ConnectionKeeper(connection *Connection) {
+    for {
+
+    }
+}
 
 func main() {
     config, err := config.ReadConfig(os.Args[1])
@@ -17,5 +92,11 @@ func main() {
         os.Exit(1)
     }
 
-    fmt.Println("My nickname:", config.Nick)
+    logfile, err := os.OpenFile(config.Logpath, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+    if err != nil {
+        fmt.Println("Error opening", config.Logpath,"for writing, error:", err.Error())
+        os.Exit(1)
+    }
+    log.SetOutput(logfile)
+
 }
