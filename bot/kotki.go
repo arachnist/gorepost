11876334d94e87120184b3cc1362f16627cd2e04
Robert Gerus <rgerus@dpd.com.pl>
2015-11-19 18:47:47 +0100

    Post kittens on :kotki

---
+++ bot/kotki.go
@@ -0,0 +1,46 @@
+// Copyright 2015 Robert S. Gerus. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package bot
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/arachnist/gorepost/irc"
+)
+
+var notAnError = errors.New("Not an error")
+
+func redirectError(*http.Request, []*http.Request) error {
+	return notAnError
+}
+
+func kotki(output chan irc.Message, msg irc.Message) {
+	if strings.Split(msg.Trailing, " ")[0] != ":kotki" {
+		return
+	}
+
+	var rmsg string
+	tr := &http.Transport{
+		TLSHandshakeTimeout:   5 * time.Second,
+		ResponseHeaderTimeout: 5 * time.Second,
+	}
+	client := &http.Client{
+		Transport:     tr,
+		CheckRedirect: redirectError,
+	}
+
+	resp, _ := client.Get("http://thecatapi.com/api/images/get?format=src&type=png")
+	rurl, _ := resp.Location()
+	rmsg = rurl.String()
+
+	output <- reply(msg, rmsg)
+}
+
+func init() {
+	addCallback("PRIVMSG", "kotki", kotki)
+}
