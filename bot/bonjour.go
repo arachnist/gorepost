24fd90d0a4844d3a4ed5925a364ab85d1a1d0a93
Robert Gerus <arachnist@i.am-a.cat>
2015-12-09 15:47:59 +0100

    Bonjourmadame is down.

--- bot/bonjour.go
+++ bot/bonjour.go
@@ -1,3 +1,5 @@
+// +build bonjourmadame
+//
 // Copyright 2015 Robert S. Gerus. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
